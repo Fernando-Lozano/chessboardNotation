b7552e1130c8d5d197906993940960562331a028
Fernando <fernando.k.lozano@gmail.com>
2022-07-29 21:28:24 -0500

    added animations

--- README.md
+++ README.md
@@ -4,21 +4,17 @@ Add an about tab and remove description.
 When the chessboard is rotated the highlighted square or column needs to be redrawn somehow
 
 Tasks:
-  learn class methods
-  add movement functions to class method
-  get ease functions for the scaling of chessboard
+  remove animations to chess board(uneccessary)
 
 Todo:
 construct a class for the chess board
   it should keep track of rotated state
   what to practice(row, column, squares)
-  functions to animate rotations
 for each row, column, square selected
   generate random question for user
   add answer to options along with three other possible answers
 if one of the options is selected check if right or wrong
 give time limit like in the mental math calculator
-add animations to rotate the board
 
 Log:
 main layout is done
--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const navLinks = document.querySelectorAll(".nav-link");
 const main = document.querySelector(".main");
 const second = document.querySelector(".second");
 const canvas = document.querySelector("canvas");
-const ctx = canvas.getContext('2d');
+const ctx = canvas.getContext("2d");
 // Get the DPR and size of the canvas
 const dpr = window.devicePixelRatio;
 const rect = canvas.getBoundingClientRect();
@@ -12,10 +12,10 @@ function fixHighRes() {
   // Set the "actual" size of the canvas
   canvas.width = rect.width * dpr;
   canvas.height = rect.height * dpr;
-  
+
   // Scale the context to ensure correct drawing operations
   // ctx.scale(dpr, dpr);
-  
+
   // Set the "drawn" size of the canvas
   canvas.style.width = `${rect.width}px`;
   canvas.style.height = `${rect.height}px`;
@@ -25,11 +25,29 @@ fixHighRes();
 /* -------------------- variables ------------------- */
 let lastActive = navLinks[0]; // main nav links on the top screen
 const padding = 20; // padding around the chess board
+let oriented = 0; // temporary: the orientation of the chessboard
 let target = 90; // temporary: used to rotate the chessboard
 
+/* -------------------- helper functions ------------------- */
+/*
+t = Time - Amount of time that has passed since the beginning of the animation. Usually starts at 0 and is slowly increased using a game loop or other update function.
+b = Beginning value - The starting point of the animation. Usually it's a static value, you can start at 0 for example.
+c = Change in value - The amount of change needed to go from starting point to end point. It's also usually a static value.
+d = Duration - Amount of time the animation will take. Usually a static value aswell.
+*/
+function easeInOutQuad(t, b, c, d) {
+  if ((t /= d / 2) < 1) return (c / 2) * t * t + b;
+  return (-c / 2) * (--t * (t - 2) - 1) + b;
+}
+function easeOutQuad(t, b, c, d) {
+  return -c * (t /= d) * (t - 2) + b;
+}
+function easeInQuad(t, b, c, d) {
+  return c * (t /= d) * t + b;
+}
 /* -------------------- main ------------------- */
-navLinks.forEach(function(link) {
-  link.addEventListener("click", function(e) {
+navLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
     e.preventDefault();
 
     // toggles active
@@ -42,7 +60,7 @@ navLinks.forEach(function(link) {
     if (link.childNodes[0].textContent === "ABOUT") {
       main.classList.add("d-none");
       second.classList.remove("d-none");
-    } else {      
+    } else {
       main.classList.remove("d-none");
       second.classList.add("d-none");
     }
@@ -50,22 +68,25 @@ navLinks.forEach(function(link) {
 });
 
 class Chessboard {
-  constructor({
-    img,
-    width,
-    height,
-    rotate = 0,
-    xRotate,
-    yRotate,
-    scale = 1
-  }) {
-    this.img = img,
-    this.width = width;
+  constructor({ img, width, height, rotate = 0, xRotate, yRotate, scale = 1 }) {
+    (this.img = img), (this.width = width);
     this.height = height;
     this.rotate = rotate;
     this.xRotate = xRotate;
     this.yRotate = yRotate;
     this.scale = scale;
+  }
+  movement() {
+    this.rotate += 90;
+  }
+  scaler(target) {
+    if (this.rotate < target / 2) {
+      // shrink
+      this.scale -= easeOutQuad(this.rotate, 0, 0.02, target / 2);
+    } else {
+      // grow
+      this.scale += easeInQuad(this.rotate, 0, 0.02, target);
+    }
   }
 }
 const chessboard = new Chessboard({
@@ -73,42 +94,43 @@ const chessboard = new Chessboard({
   width: canvas.width - padding * 2,
   height: canvas.height - padding * 2,
   xRotate: canvas.width / 2,
-  yRotate: canvas.height / 2
+  yRotate: canvas.height / 2,
 });
-chessboard.img.addEventListener('load', function() {
-  window.requestAnimationFrame(render);
-  console.log(chessboard.xRotate, chessboard.yRotate);
-}, false);
-chessboard.img.src = './images/chessboard.png'; // Set source path
+chessboard.img.addEventListener(
+  "load",
+  function () {
+      ctx.drawImage(
+        chessboard.img,
+        padding,
+        padding,
+        chessboard.width,
+        chessboard.height
+      );
+    setTimeout(render, 5000)
+    // window.requestAnimationFrame(render);
+  },
+  false
+);
+chessboard.img.src = "./images/chessboard.png"; // Set source path
 
-function render(){
+function render() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  movement(chessboard);
-  scale(chessboard, target);
+  chessboard.movement(chessboard);
+  chessboard.scaler(target);
   ctx.save();
   ctx.translate(chessboard.xRotate, chessboard.yRotate);
-  ctx.rotate(chessboard.rotate * Math.PI / 180);
+  ctx.rotate((chessboard.rotate * Math.PI) / 180);
   ctx.scale(chessboard.scale, chessboard.scale);
   ctx.translate(-chessboard.xRotate, -chessboard.yRotate);
-  ctx.drawImage(chessboard.img, padding, padding, chessboard.width, chessboard.height);
+  ctx.drawImage(
+    chessboard.img,
+    padding,
+    padding,
+    chessboard.width,
+    chessboard.height
+  );
   ctx.restore();
   if (!(chessboard.rotate === target)) {
     window.requestAnimationFrame(render);
-  }
-}
-
-function movement(shape) {
-  shape.rotate += 1;
-  // shape.xPosition += 0.1;
-}
-
-// add ease in and out functions to smoothen this out... research time
-function scale(shape, target) {
-  if (shape.rotate <= target / 2) {
-    // shrink
-    shape.scale -= 0.01;
-  } else {
-    // grow
-    shape.scale += 0.01;
   }
 }
